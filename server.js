01c706bb09c3724451fabd7fa26efffdf965a1c4
Josh Moran <josh@lovingfamily.co.uk>
2024-11-05 15:15:47 +0000

    Updates

--- server.js
+++ server.js
@@ -17,8 +17,50 @@ console.log('options: ', options);
 
 const pool = mysql.createPool(options);
 
-app.get('/', (req, res) => {
-    pool.query('SELECT * FROM ', (error, result) => {
+app.use(express.json());
+
+app.post("/ticket", (req, res) => {
+    const ticket = req.body;
+    pool.query('INSERT INTO ticket (summary, priority, status ) values ( ?, ?, ? )', [ticket.summary, ticket.priority, ticket.status], (error, result) => {
+        if( error ) {
+            console.error(error);
+            res.send(error);
+            return;
+        }
+
+        res.send({ id: result.insertId, ...ticket });
+    });
+});
+
+app.put("/ticket", (req, res) => {
+    const ticket = req.body;
+    pool.query('UPDATE ticket SET ? where id = ? ', [ticket, ticket.id], (error, result) => {
+        if( error ) {
+            console.error(error);
+            res.send(error);
+            return;
+        }
+
+        res.send(ticket);
+    });
+})
+
+app.delete('/ticket/:id', (req, res) => {
+    const id = req.params.id;
+    pool.query('delete FROM ticket where id = ?', [id], (error, result) => {
+        if (error){
+            console.error(error);
+            res.send(error);
+            return;
+        } 
+
+        res.send("success");
+    });
+});
+
+app.get('/ticket/:id', (req, res) => {
+    const id = req.params.id;
+    pool.query('SELECT * FROM ticket where id = ?', [id], (error, result) => {
         if (error){
             console.error(error);
             res.send(error);
@@ -28,6 +70,18 @@ app.get('/', (req, res) => {
         res.send(result);
     });
 });
+
+app.get("/all", (req, res) => {
+    pool.query('SELECT * FROM ticket', (error, result) => {
+        if (error){
+            console.error(error);
+            res.send(error);
+            return;
+        } 
+
+        res.send(result);
+    });
+})
 
 app.listen(3000, () => {
     console.log('server started on port 3000');
